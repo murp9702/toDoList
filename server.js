f654e1f44b014637fe47d452ec7532ad959bf582
Brennen Murphy <brennenamurphy@gmail.com>
2017-11-27 20:26:47 -0700

    bug fixed

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
-var cors = require('cors')
 var app = express();
 
 var MongoClient = mongo.MongoClient;
@@ -22,21 +21,19 @@ MongoClient.connect('mongodb://localhost:27017/toDo', function(err, db) {
     res.sendFile('/html/index.html', {
       root: './public'
     });
-    //   next();
   });
 
   app.get('/getData', function(req, res, next) {
     var objectForDatabase = {}
     var dbContents = db.collection('list').find({}).toArray(
-      function(err,docs){
-      if (err) {
-        console.log(err)
-        res.send("You done messed up!")
+      function(err, docs) {
+        if (err) {
+          console.log(err)
+          res.send("You done messed up!")
+        } else {
+          res.send(docs)
+        }
       }
-      else {
-        res.send(docs)
-      }
-    }
     )
   })
 
@@ -44,16 +41,34 @@ MongoClient.connect('mongodb://localhost:27017/toDo', function(err, db) {
 
 
   app.post('/submit', function(req, res) {
-    db.collection('list').insert({text: req.body.text, complete: req.body.complete}, function(err) {
+    db.collection('list').insert({
+      text: req.body.text,
+      complete: req.body.complete
+    }, function(err) {
       if (err) {
         res.send('You done messed up!');
       } else {
-        console.log(req.body);
         res.send('You did it');
       }
     });
-    db.collection('list').update({ complete: "true" }, { $set: {complete: true} }, { multi: true })
-    db.collection('list').update({ complete: "false" }, { $set: {complete: false} }, { multi: true })
+    db.collection('list').update({
+      complete: "true"
+    }, {
+      $set: {
+        complete: true
+      }
+    }, {
+      multi: true
+    })
+    db.collection('list').update({
+      complete: "false"
+    }, {
+      $set: {
+        complete: false
+      }
+    }, {
+      multi: true
+    })
   });
 
   app.post('/complete', function(req, res) {
@@ -69,13 +84,16 @@ MongoClient.connect('mongodb://localhost:27017/toDo', function(err, db) {
         res.send('You done messed up: ' + err);
       } else {
         res.send('complete');
-        console.log(req);
       }
     });
   });
 
   app.post('/delete', function(req, res) {
-    db.collection('list').remove({text: {$eq: req.body.text}})
+    db.collection('list').remove({
+      text: {
+        $eq: req.body.text
+      }
+    })
     console.log(req.body.text);
     res.send('delete')
   })
